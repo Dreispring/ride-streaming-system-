e1f4e9821b94667ecaec684f99c1fea92783246e
Nguyen Xuan Ba <nguyenxuanba1998@gmail.com>
2026-05-11 08:56:24 +0000

    update project before sync with remote

--- api/kafka_consumer.py
+++ api/kafka_consumer.py
@@ -1,14 +1,26 @@
 from kafka import KafkaConsumer
 import json
+import time
 
-consumer = KafkaConsumer(
-    'ride-events',
-    bootstrap_servers='kafka:9092',
-    auto_offset_reset='earliest',
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
-)
+while True:
+    try:
+        consumer = KafkaConsumer(
+            "ride-events",
+            bootstrap_servers="kafka:9092",
+            auto_offset_reset="latest",
+            group_id="ride-consumer-group",
+            value_deserializer=lambda x: json.loads(x.decode("utf-8"))
+        )
 
-print("🚀 Consumer started...")
+        print("✅ Connected to Kafka")
+        break
+
+    except Exception as e:
+        print(f"⏳ Waiting for Kafka... {e}")
+        time.sleep(5)
+
+print("🚀 Waiting for messages...")
 
 for message in consumer:
-    print("Received event:", message.value)
+    print("📩 Received event:")
+    print(message.value)
